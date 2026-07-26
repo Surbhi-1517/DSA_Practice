6ec33dcfa5b37945e37ac0e675206821f88673a3
Surbhi kumari <1517surbhi@gmail.com>
2026-07-26 12:19:36 +0530

    Update Test.java

--- 008/Test.java
+++ 008/Test.java
@@ -1,6 +1,15 @@
+import java.util.*;
+public static void main(String[]args){
+     int n= 5;
+     System.out.println(" ");
+     for(int i = n; i >= 1; i--){
+          for(int j = 1; j <= n - i; j ++) {
+               System.out.print("");
+          }
+     }
 
-
-
+     
+     
 
 
 
