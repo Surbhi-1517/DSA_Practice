64376f1749f2a5a8350a6cb5ee4480f80e5c2dc8
Surbhi kumari <1517surbhi@gmail.com>
2026-08-01 21:16:53 +0530

    Create Test.java

---
+++ 014/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char)(64 + j));
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+#Output
+ABCDE
+ABCD
+ABC
+AB
+A
