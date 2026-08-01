2f531742e36ff873eb92472535233d2d1ea5435c
Surbhi kumari <1517surbhi@gmail.com>
2026-08-01 20:44:00 +0530

    Create Test.java

---
+++ 010/Test.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        int n = 5;
+
+        for(int i = 1; i <= n; i++){
+            int start;
+            if(i % 2 == 0){
+                start = 0;
+            }
+            else{
+                start = 1;
+            }
+
+            for(int j = 1; j <= i; j++){
+                System.out.print(start + " ");
+                if(start == 1){
+                    start = 0;
+                }
+                else{
+                    start = 1;
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
+
+
+
+#Output 
+
+
+  1 
+  0 1
+  1 0 1
+  0 1 0 1
+  1 0 1 0 1
+  
